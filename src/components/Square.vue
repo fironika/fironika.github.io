e17d665a1dead0a10736678a25f038d91db971c5
Sprucek, Dan <dan.sprucek@cvut.cz>
2020-12-30 15:56:10 +0100

    Set up provisional squares; Add Bootstrap

---
+++ src/components/Square.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="square" :style="{ width: `${width}px` }"></div>
+</template>
+
+<script>
+export default {
+  name: "Square",
+  props: {
+    width: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.square {
+  border: 5px solid lime;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="ml-5 mt-4">
+    <div
+      class="d-flex"
+      v-for="n in rows"
+      :key="n"
+      :style="{ height: `${height[n - 1]}px` }"
+    >
+      <square v-for="n in columns" :key="n" :width="width[n - 1]" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Square from "../components/Square";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Square
+  },
+  data() {
+    return {
+      columns: 8,
+      rows: 6,
+      height: [160, 110, 60, 165, 60, 130],
+      width: [160, 90, 280, 160, 270, 100, 190, 170]
+    };
   }
 };
 </script>
+
+<style lang="scss" scoped></style>
